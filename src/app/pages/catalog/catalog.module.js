774b471867238c63019357c7a3cddba786326882
kshakirov <kshakirov@zoral.com.ua>
2016-11-22 18:15:25 +0200

    WIP #3 started attribute and attribute sets

---
+++ src/app/pages/catalog/catalog.module.js
@@ -0,0 +1,100 @@
+(function () {
+    'use strict';
+
+    var catalog = angular.module('BlurAdmin.pages.catalog', ['ngCookies']);
+
+
+
+
+    catalog.config(routeConfig);
+
+    /** @ngInject */
+    function routeConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('catalog', {
+                url: '/catalog',
+                template : '<ui-view></ui-view>',
+                abstract: true,
+                controller: 'CatalogController',
+                title: 'Catalog',
+                sidebarMeta: {
+                    icon: 'ion-grid',
+                    order: 300,
+                },
+            }).state('catalog.attribute', {
+            url: '/attribute',
+            templateUrl: 'app/pages/catalog/attribute/attribute.html',
+            title: 'Attributes',
+            sidebarMeta: {
+                order: 0,
+            },
+        }).state('catalog.attribute_set', {
+            url: '/attribute_set',
+            templateUrl: 'app/pages/catalog/attribute_set/attribute_set.html',
+            title: 'Attribute Sets',
+            sidebarMeta: {
+                order: 100,
+            },
+        });
+        $urlRouterProvider.when('/catalog','/catalog/attribute');
+    };
+
+    catalog.controller('CatalogController', function ($scope, $http, $window) {
+
+        $scope.attributeTablePageSize = 10;
+        $scope.attributeSetTablePageSize = 10;
+
+        $scope.initAttributes = function () {
+            console.log("INIT");
+            $http.get("/admin/attribute/").then(function (promise) {
+                $scope.attributes = promise.data;
+                $scope.attributesReady = true;
+            }, function (error) {
+                console.log(error);
+                //$window.location.href = '/auth.html';
+            })
+
+        };
+
+        $scope.editSet = function (code) {
+            $http.get("/admin/attribute_set/" + code).then(function (promise) {
+                $scope.attribute_set = promise.data[0];
+                $scope.editAttributeSet = true;
+            }, function (error) {
+                console.log(error);
+                //$window.location.href = '/auth.html';
+            })
+        }
+
+        $scope.saveSet = function (set) {
+            console.log(set)
+            // $http.post("/admin/attribute_set/", set).then(function (promise) {
+            //     console.log(promise)
+            //     $scope.editAttributeSet = true;
+            // }, function (error) {
+            //     console.log(error);
+            //     //$window.location.href = '/auth.html';
+            // })
+        }
+
+        $scope.initAttributeSets = function () {
+            console.log("INIT as");
+            $http.get("/admin/attribute_set/").then(function (promise) {
+                $scope.attributeSets = promise.data;
+                $scope.attributeSetsReady = true;
+                $scope.editAttributeSet = false;
+            }, function (error) {
+                console.log(error);
+                //$window.location.href = '/auth.html';
+            })
+
+        }
+    })
+
+
+
+})();/**
+ * Created by kshakirov on 11/18/16.
+ */
+
+
--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -10,6 +10,7 @@
 
     'BlurAdmin.pages.dashboard',
     'BlurAdmin.pages.orders',
+    'BlurAdmin.pages.catalog',
     'BlurAdmin.pages.ui',
     'BlurAdmin.pages.components',
     'BlurAdmin.pages.form',
