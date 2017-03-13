1a0bb1a8cf54d81c5b900e877023bb4bca489dfc
kshakirov <kshakirov@zoral.com.ua>
2017-03-13 14:42:38 +0200

    WIP #2 Customer new functionality create order

--- src/app/pages/customers/customers_controller.js
+++ src/app/pages/customers/customers_controller.js
@@ -25,6 +25,12 @@ function _create_customers_controller($scope, $http,
         })
     }
 
+    function create_order(customer_id) {
+        return $http.put(url_prefix + "customer/" + customer_id + "/order/new/", {}).then(function (promise) {
+            return promise;
+        })
+    }
+
 
     function reset_password(email) {
         var data = {email: email};
@@ -65,10 +71,17 @@ function _create_customers_controller($scope, $http,
         save_customer(customer_data).then(function (promise) {
             $scope.customer_saved_data = promise.data;
         })
-    }
+    };
+
     $scope.reset_password = function (email) {
         reset_password(email).then(function (promise) {
             $scope.customer_reset_password = promise.data;
+        })
+    };
+
+    $scope.create_order = function (customer_id) {
+        create_order(customer_id).then(function (promise) {
+            $scope.customer_created_order = promise.data;
         })
     }
     
