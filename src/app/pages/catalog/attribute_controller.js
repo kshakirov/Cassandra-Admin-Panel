21742f1dbad1aea8198b95c3ae652ff1fa4c5982
kshakirov <kshakirov@zoral.com.ua>
2017-05-18 16:00:02 +0300

    WIP #27 started attribute edit creation

--- src/app/pages/catalog/attribute_controller.js
+++ src/app/pages/catalog/attribute_controller.js
@@ -2,18 +2,36 @@ function _create_attribute_controller($scope, $http, $stateParams, $window) {
     $scope.smartTablePageSize = 10;
     $scope.stage = false;
 
+    $scope.types = ['decimal', 'int', 'text']
+    $scope.entities = ['product', 'order', 'customer']
+    $scope.units = ['INCHES', 'DEGREES', 'gr']
+
     function not_authorized (error, w) {
         if (error.status == 401) {
             w.location.href = '/auth.html';
         }
     }
-
     
-    function _init_edit(id) {
+    $scope.addOption = function (attribute_options) {
+        if(attribute_options){
+            attribute_options.push("option")
+        }else{
+            attribute_options = ['option']
+        }
+    }
+
+    $scope.deleteOption = function (attribute_options, index) {
+        attribute_options = attribute_options.splice(index,1);
+    }
+    
+
+
+function _init_edit(id) {
         $http.get('/admin/attribute/' + id).then(function (promise) {
             $scope.attribute = promise.data;
         })
     }
+    
 
     function _init_list() {
         $http.get("/admin/attribute/").then(function (promise) {
@@ -26,10 +44,14 @@ function _create_attribute_controller($scope, $http, $stateParams, $window) {
     }
 
     $scope.init = function () {
-        if ($stateParams.id) {
+        if ($stateParams.id == 'new') {
+            _init_edit($stateParams.id)
+            $scope.new = true
+        }else  if ($stateParams.id) {
             _init_edit($stateParams.id)
             $scope.stage = true
-        } else {
+        }
+        else {
             $scope.stage = false;
             _init_list();
         }
