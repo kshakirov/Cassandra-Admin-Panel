7c43571359bd598bdceff59578886622bcbd78a8
kshakirov <kshakirov@zoral.com.ua>
2017-05-18 13:38:42 +0300

    DONE #26 if one is not singed in gets redirected to the sign in page

--- src/app/app.js
+++ src/app/app.js
@@ -1,18 +1,52 @@
 'use strict';
 
-angular.module('BlurAdmin', [
-  'ngAnimate',
-  'ui.bootstrap',
-  'ui.sortable',
-  'ui.router',
-  'ngTouch',
-  'toastr',
-  'smart-table',
-  "xeditable",
-  'ui.slimscroll',
-  'ngJsTree',
-  'angular-progress-button-styles',
+var app = angular.module('BlurAdmin', [
+    'ngAnimate',
+    'ui.bootstrap',
+    'ui.sortable',
+    'ui.router',
+    'ngTouch',
+    'toastr',
+    'smart-table',
+    "xeditable",
+    'ui.slimscroll',
+    'ngJsTree',
+    'angular-progress-button-styles',
 
-  'BlurAdmin.theme',
-  'BlurAdmin.pages'
+    'BlurAdmin.theme',
+    'BlurAdmin.pages'
 ]);
+
+app.factory('sessionInjector', function ($cookies) {
+    var sessionInjector = {
+        request: function (config) {
+
+            config.headers['Authorization'] = "Bearer " + $cookies.getObject('token');
+
+            return config;
+        }
+    };
+    return sessionInjector;
+});
+app.config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('sessionInjector');
+}]);
+
+app.controller("SignInController", function ($scope, $http, $window) {
+    function _get_user_proile() {
+        return $http.post("/admin/profile", {}).then(function (promise) {
+            return promise.data;
+        })
+    }
+
+    $scope.init = function () {
+        _get_user_proile().then(function (promise) {
+            console.log()
+        }, function (error) {
+            console.log(error)
+            if (error.status == 401) {
+                $window.location.href = '/auth.html';
+            }
+        })
+    }
+})
