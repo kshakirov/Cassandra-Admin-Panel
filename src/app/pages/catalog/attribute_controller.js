e05e127823ff1f985065bc7e9181c62f0ff14630
kshakirov <kshakirov@zoral.com.ua>
2017-05-18 20:29:37 +0300

    WIP #27 save update

--- src/app/pages/catalog/attribute_controller.js
+++ src/app/pages/catalog/attribute_controller.js
@@ -6,6 +6,26 @@ function _create_attribute_controller($scope, $http, $stateParams, $window) {
     $scope.entities = ['product', 'order', 'customer']
     $scope.units = ['INCHES', 'DEGREES', 'gr']
 
+    $scope.cancel_alert = function (key) {
+        this[key]['flag'] = false;
+    }
+
+    function notify_success(message) {
+        var success = {
+            flag: true,
+            message: message
+        }
+        return success;
+    }
+
+    function notify_success(message) {
+        var success = {
+            flag: true,
+            message: message
+        }
+        return success;
+    }
+    
     function not_authorized (error, w) {
         if (error.status == 401) {
             w.location.href = '/auth.html';
@@ -43,9 +63,30 @@ function _init_edit(id) {
         )
     }
 
+    function _create(attribute) {
+        return $http.post('/admin/attribute/', attribute).then(function (promise) {
+            return promise.data;
+        })
+    }
+    
+
+    $scope.readyToSubmit = function (attribute) {
+        if(attribute.type && attribute.entity && attribute.label){
+            return false;
+        }
+        return true;
+    }
+    
+    $scope.createAttribute = function (attribute) {
+        _create(attribute).then(function (promise) {
+            $scope.success = notify_success("Attribute Successfully Saved")
+        }, function (error) {
+            $scope.error = render_error(error)
+        })
+    }
+    
     $scope.init = function () {
         if ($stateParams.id == 'new') {
-            _init_edit($stateParams.id)
             $scope.new = true
         }else  if ($stateParams.id) {
             _init_edit($stateParams.id)
